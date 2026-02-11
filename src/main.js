591bc482a34c7e8ab648bd6b90caeb9eda3fa6f2
zpirhadi <zahrapirhadi27@gmail.com>
2026-02-11 23:03:18 +0330

    final

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-const taskInputEl =document.getElementById("task-input");
-const addTaskEl =document.getElementById("button");
-const todoListEl =document.getElementById("todo-list");
-const itemsLeftEl =document.getElementById("items-left");
-const clearEl =document.getElementById("clean");
-
+const taskInputEl = document.getElementById("task-input");
+const addTaskEl = document.getElementById("button");
+const todoListEl = document.getElementById("todo-list");
+const itemsLeftEl = document.getElementById("items-left");
+const clearEl = document.getElementById("clean");
 
 // console.log("taskInputEl");
 // console.log("addTaskEl");
@@ -16,25 +15,25 @@ const option = { weekday: "long", month: "short", day: "numeric" };
 const dataString = today.toLocaleDateString("en-us", option);
 document.getElementById("date").textContent = dataString;
 
-
 const task = [];
 addTaskEl.addEventListener("click", () => {
   const taskValue = taskInputEl.value;
 
-
   task.push(taskValue);
-  
-  if (taskInputEl ===""|| taskInputEl === " ") {
+
+  if (taskValue === "" || taskValue === " ") {
     return;
   }
-
 
   todoListEl.innerHTML = "";
 
   for (let i = 0; i < task.length; i++) {
-    todoListEl.innerhtml += `<li class="p-1"><input type="checkbox"><span> ${task[i]} </span></li>`;
+    todoListEl.innerHTML += `<li class="p-1"><input type="checkbox"><span> ${task[i]} </span></li>`;
   }
   taskInputEl.value = "";
   itemsLeftEl.innerText = `${task.length} items Left `;
-
+});
+clearEl.addEventListener("click", () => {
+  task.length = 0;
+  todoListEl.innerHTML = "";
 });
